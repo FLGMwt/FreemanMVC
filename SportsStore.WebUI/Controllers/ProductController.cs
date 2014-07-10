fd97f9b94c6be84aeaf8bfac491f661cc992125b
FLGMwt <ryan.stelly@live.com>
2014-07-10 13:58:17 -0500

    Basic product list w/ EF

---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+        public int PageSize;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            repository = productRepository;
+        }
+
+        public ViewResult List(int page = 1)
+        {
+            return View(
+                repository.Products
+                .OrderBy(x => x.ProductID)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize));
+        }
+	}
+}
--- SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Moq;
+using SportsStore.Domain.Entities;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
 
 namespace SportsStore.WebUI.Infrastructure
 {
@@ -19,7 +23,15 @@ namespace SportsStore.WebUI.Infrastructure
 
         private void AddBindings()
         {
-            // bindings
+            //var mock = new Mock<IProductRepository>();
+            //mock.Setup(m => m.Products).Returns(new[] {
+            //    new Product { Name = "Football", Price = 25},
+            //    new Product { Name = "Surf board", Price = 179},
+            //    new Product { Name = "Running shoes", Price = 95}
+            //});
+
+            //kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+            kernel.Bind<IProductRepository>().To<EFProductRepository>();
         }
 
         public object GetService(Type serviceType)
