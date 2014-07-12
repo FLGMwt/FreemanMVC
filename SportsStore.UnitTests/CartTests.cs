de296fb68c0213c13027372ca57b5cc992687361
FLGMwt <ryan.stelly@live.com>
2014-07-12 13:32:54 -0500

    view cart and add items to cart

---
+++ SportsStore.UnitTests/CartTests.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportsStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class CartTests
+    {
+        [TestMethod]
+        public void Can_Add_New_Lines()
+        {
+            var p1 = new Product { ProductID = 1, Name = "P1" };
+            var p2 = new Product { ProductID = 2, Name = "P2" };
+            var cart = new Cart();
+
+            cart.AddItem(p1, 1);
+            cart.AddItem(p2, 1);
+            var results = cart.Lines.ToArray();
+
+            Assert.AreEqual(results.Length, 2);
+            Assert.AreEqual(results[0].Product, p1);
+            Assert.AreEqual(results[1].Product, p2);
+        }
+
+        [TestMethod]
+        public void Can_Add_Quantity_For_Existing_Lines()
+        {
+            var p1 = new Product { ProductID = 1, Name = "P1" };
+            var p2 = new Product { ProductID = 2, Name = "P2" };
+            var cart = new Cart();
+
+            cart.AddItem(p1, 1);
+            cart.AddItem(p2, 1);
+            cart.AddItem(p1, 10);
+            var results = cart.Lines.ToArray();
+
+            Assert.AreEqual(results.Length, 2);
+            Assert.AreEqual(results[0].Quantity, 11);
+            Assert.AreEqual(results[1].Quantity, 1);
+        }
+
+        [TestMethod]
+        public void Can_Remove_Line()
+        {
+            var p1 = new Product { ProductID = 1, Name = "P1" };
+            var p2 = new Product { ProductID = 2, Name = "P2" };
+            var p3 = new Product { ProductID = 3, Name = "P3" };
+            var cart = new Cart();
+            cart.AddItem(p1, 1);
+            cart.AddItem(p2, 1);
+            cart.AddItem(p3, 5);
+            cart.AddItem(p2, 1);
+
+            cart.RemoveLine(p2);
+
+            Assert.AreEqual(cart.Lines.Count(c => c.Product == p2), 0);
+            Assert.AreEqual(cart.Lines.Count(), 2);
+        }
+
+        [TestMethod]
+        public void Calculate_Cart_Total()
+        {
+            var p1 = new Product { ProductID = 1, Name = "P1", Price = 100M };
+            var p2 = new Product { ProductID = 2, Name = "P2", Price = 50M };
+            var cart = new Cart();
+
+            cart.AddItem(p1, 1);
+            cart.AddItem(p2, 1);
+            cart.AddItem(p1, 3);
+            var result = cart.ComputeTotalValue();
+
+            Assert.AreEqual(result, 450M);
+        }
+
+        [TestMethod]
+        public void Can_Clear_Contents()
+        {
+            var p1 = new Product { ProductID = 1, Name = "P1", Price = 100M };
+            var p2 = new Product { ProductID = 2, Name = "P2", Price = 50M };
+            var cart = new Cart();
+
+            cart.AddItem(p1, 1);
+            cart.AddItem(p2, 1);
+            cart.Clear();
+
+            Assert.AreEqual(cart.Lines.Count(), 0);
+        }
+    }
+}
