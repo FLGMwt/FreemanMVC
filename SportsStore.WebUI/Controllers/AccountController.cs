aa01bd05ff9ea1cd4b353e63440593bdc717a85f
FLGMwt <ryan.stelly@live.com>
2014-07-15 22:05:00 -0500

    Revert "Fix AccountController"
    This reverts commit 1ecf36713c5c90f3306c4a186bf525a759210e5f.

--- SportsStore.WebUI/Controllers/AccountController.cs
+++ SportsStore.WebUI/Controllers/AccountController.cs
@@ -8,7 +8,7 @@ using System.Web.Mvc;
 
 namespace SportsStore.WebUI.Controllers
 {
-    public class AccountController : Controller
+    public class AccountController
     {
         IAuthProvider authProvider;
 
--- SportsStore.WebUI/Infrastructure/Abstract/IAuthProvider.cs
+++ SportsStore.WebUI/Infrastructure/Abstract/IAuthProvider.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SportsStore.WebUI.Infrastructure.Abstract
 {
-    public interface IAuthProvider
+    interface IAuthProvider
     {
         bool Authenticate(string username, string password);
     }
